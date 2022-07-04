a337f120fd5935d353b5f3112e6e45dc066e55f4
Nikita Boyarskikh <n.boyarskikh@getdisco.com>
2022-07-05 00:37:20 +0300

    WIP: Index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div></div>
+import styles from './Index.module.css'
+
+const Index = () => {
+  return (
+    <>
+      <section className={styles.hero}>
+        <h1 className={styles.header}>Переклейка дисплея любого iphone</h1>
+        <div className={styles.subheader}>
+          Oригинальные запчасти<br />
+          Разумные цены<br />
+          Выезд
+        </div>
+      </section>
+      <section className={styles.advantages}>
+
+      </section>
+    </>
+  )
 }
+
+export default Index
